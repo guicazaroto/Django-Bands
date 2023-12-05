ac84dab9d9f72dfb6985c00819c6a8ed45a90fc7
Guilherme Guimarães Cazaroto <gui.cazaroto@gmail.com>
2023-12-04 21:50:54 -0300

    add delete

--- band/urls.py
+++ band/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from band.views import home_band, edit_band, update_band
+from band.views import home_band, edit_band, update_band, add_band, save_band, delete_band
 
 urlpatterns = [
     path('', home_band),
     path('<int:id>', edit_band, name='edit_band'),
     path('update/<int:id>', update_band, name='update_band'),
+    path('add/', add_band, name='add_band'),
+    path('save/', save_band, name='save_band'),
+    path('delete/<int:id>', delete_band, name='delete_band')
 ]
--- band/views.py
+++ band/views.py
@@ -20,3 +20,26 @@ def update_band(request, id):
         band.save()
 
         return redirect(home_band)
+    
+def add_band(request):
+    return render(request, 'band/add.html')
+
+
+def save_band(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        genre = request.POST.get('genre')
+
+        Band.objects.create(
+            name=name,
+            genre=genre,
+        )
+        bands = Band.objects.all()
+
+        return render(request, 'band/index.html', { 'bands': bands })
+
+def delete_band(request, id):
+    band = Band.objects.get(id=id)
+    band.delete()
+    
+    return redirect(home_band)
