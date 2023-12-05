923f8bb37c28aa7e09b2bfde61d4e0a308809440
Guilherme Guimarães Cazaroto <gui.cazaroto@gmail.com>
2023-12-04 21:32:37 -0300

    save

--- band/urls.py
+++ band/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from band.views import home_band
+from band.views import home_band, edit_band, update_band
 
 urlpatterns = [
-    path('', home_band)
+    path('', home_band),
+    path('<int:id>', edit_band, name='edit_band'),
+    path('update/<int:id>', update_band, name='update_band'),
 ]
--- band/views.py
+++ band/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from band.models import Band
 
 # Create your views here.
 def home_band(request):
     bands = Band.objects.all()
     return render(request, 'band/index.html', { 'bands': bands })
+
+def edit_band(request, id):
+    band = Band.objects.get(id=id)
+    return render(request, 'band/edit.html', { 'band': band })
+
+def update_band(request, id):
+    if request.method == 'POST':
+        band = Band.objects.get(id=id)
+
+        band.name = request.POST.get('name')
+        band.genre = request.POST.get('genre')
+
+        band.save()
+
+        return redirect(home_band)
